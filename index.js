02b272561592ec0ad1f002f0c5fa4fa13cf73ce0
phryszlo <phryszlo@gmail.com>
2022-09-17 14:40:13 -0400

    based on a codegames scrabble, trying to figure out readline

--- index.js
+++ index.js
@@ -36,39 +36,3 @@ const main = async () => {
 }
 
 main();
-
-// rl.question('wtf?', (answer) => {
-//   console.log('ok');
-//   D.push(answer)
-// })
-
-// const D = [];
-
-// let N;
-// // rl.question('# words in dictionary:  ', (answer) => {
-//   N = parseInt(answer);
-//   // rl.pause();
-// // });
-
-// for (let i = 0; i < N; i++) {
-//   let w = [];
-//   let W = rl();
-//   // rl.question(`Word #${i + 1}`, (answer) => {
-//     W.forEach(l => {
-//       w.push(l);
-//     // })
-//     D.push(w);
-//   });
-//   // rl.pause();
-// }
-
-// // rl.question('what the hell is this bit? ', (answer) => {
-//   const LETTERS = answer;
-//   rl.close();
-// });
-
-// D.forEach(word => {
-//   console.log(word.join(''));
-// })
-
-// // console.log('invalid word');
